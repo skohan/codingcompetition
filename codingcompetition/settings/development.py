37014db4726c075666e69a5a6b69a9f0267653aa
Sohan <sohan@email.com>
2021-05-05 11:10:56 +0530

    Development ready

--- codingcompetition/settings/development.py
+++ codingcompetition/settings/development.py
@@ -1,5 +1,11 @@
 
-
 from .base import *
 
 DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'tmp/' 'db.sqlite3',
+    }
+}
